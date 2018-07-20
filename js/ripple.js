9ee631b0e443c580a82a53da9ac98e7e46be3314
Timothy Cyrus <tcyrus@users.noreply.github.com>
2018-07-20 15:20:50 -0400

    Update ripple.js
    Simplified code
    * on('click') can be replaced with click()
    * Class can be included at element creation (no need for addClass())
    * Fix Code Style Inconsistencies

--- js/ripple.js
+++ js/ripple.js
@@ -3,37 +3,30 @@
 // http://codepen.io/Craigtut/pen/dIfzv
 
 (function (window, $) {
+  $(function () {
+    $('.ripple, .button, .tabs-title').click(function (event) {
 
-  $(function() {
+      var btnOffset = $(this).offset(),
+          xPos = event.pageX - btnOffset.left,
+          yPos = event.pageY - btnOffset.top;
 
+      var $div = $('<div/>', { class: 'ripple-effect' });
 
-    $('.ripple, .button, .tabs-title').on('click', function (event) {
+      var $ripple = $('.ripple-effect');
 
-      var $div = $('<div/>'),
-          btnOffset = $(this).offset(),
-      		xPos = event.pageX - btnOffset.left,
-      		yPos = event.pageY - btnOffset.top;
+      $ripple.css('height', $(this).height());
+      $ripple.css('width', $(this).height());
 
+      $div.css({
+        top: yPos - ($ripple.height() / 2),
+        left: xPos - ($ripple.width() / 2),
+        background: $(this).data('ripple-color')
+      }).appendTo($(this));
 
-
-      $div.addClass('ripple-effect');
-      var $ripple = $(".ripple-effect");
-
-      $ripple.css("height", $(this).height());
-      $ripple.css("width", $(this).height());
-      $div
-        .css({
-          top: yPos - ($ripple.height()/2),
-          left: xPos - ($ripple.width()/2),
-          background: $(this).data("ripple-color")
-        })
-        .appendTo($(this));
-
-      window.setTimeout(function(){
+      window.setTimeout(function () {
         $div.remove();
       }, 2000);
+
     });
-
   });
-
 })(window, jQuery);
